1d8984ef432712b4c3a50ca38cdd7a6013834fdc
bknie1 <brandonknieriem26@gmail.com>
2017-06-24 16:38:01 -0400

    Initial commit.

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+	// create a LatLng object containing the coordinate for the center of the map
+	var latlng = new google.maps.LatLng(41.218603, -73.215365);
+
+	// prepare the map properties
+	var options = {
+	zoom: 15,
+	center: latlng,
+	mapTypeId: google.maps.MapTypeId.ROADMAP,
+	navigationControl: true,
+	mapTypeControl: false,
+	scrollwheel: false,
+	disableDoubleClickZoom: true
+	};
+
+	// Initialize the map object
+	var map = new google.maps.Map(document.getElementById('contact'), options);
+
+	// Add Marker
+	var marker1 = new google.maps.Marker( {
+	position: latlng, map: map
+	});
+
+	// Add listener for a click on the pin.
+	google.maps.event.addListener(marker1, 'click', function() {
+	infowindow.open(map, marker1);
+	});
+
+	// Add information window.
+	var infowindow = new google.maps.InfoWindow({
+	content:  '<div class="info"><strong>Fashion De La Rosa</strong><br>4267 Main St.<br>Bridgeport, CT 06606<br><br><strong>Phone: </strong>(203) 936-7672</div>'
+	});
+});
